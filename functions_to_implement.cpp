b4dc96b720a8d28b6714a5f609e837d432b67ca1
Jack <collierjack101@gmail.com>
2020-09-22 14:13:34 -0600

    Update functions_to_implement.cpp

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -110,7 +110,12 @@ double Sign(double num);
 
 
 // adds n to each element of the vector
-std::vector<int> AddN(std::vector<int>, int n);
+std::vector<int> AddN(std::vector<int>, int n){
+	for(int i=0;i<(int)v.size();i++){
+		v[i]+=n;
+	}
+	return v;
+}
 
 // adds n to each element of the vector
 std::vector<double> AddN(std::vector<double>, double n);
