76b4a641e8e9705be5738c8ca6c6c542e7664b82
Jack <collierjack101@gmail.com>
2020-09-24 12:59:42 -0600

    Added Test_Case for addN

--- test.cpp
+++ test.cpp
@@ -5,3 +5,28 @@
 
 
 // adding commit
+TEST_CASE("incrementing values in integer vector", "[addN]"){
+	std::vector<int> v{1,2,3,5,6,7,8,10};
+
+	SECTION("checking with +ve n"){
+		int n=5;
+		std::vector<int> res = AddN(v, n);
+		srand(time(NULL));
+		int random = rand()%v.size();
+		REQUIRE(v.size() == res.size());
+		REQUIRE( res[0] == 6 );
+		REQUIRE( res[v.size()-1] == 15 );
+		REQUIRE(res[random] == v[random]+n);
+	}
+	SECTION("checking with -ve n"){
+		int n=-5;
+		std::vector<int> res = AddN(v, n);
+		srand(time(NULL));
+		int random = rand()%v.size();
+		REQUIRE(v.size() == res.size());
+		REQUIRE( res[0] == -4 );
+		REQUIRE( res[v.size()-1] == 5 );
+		REQUIRE(res[random] == v[random]+n);
+	}
+	
+}
