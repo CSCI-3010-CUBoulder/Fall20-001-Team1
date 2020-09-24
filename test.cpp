9a7864dd1dea1df16b5011a4613687ae0e3e4aeb
picabotpi <59852986+picabotpi@users.noreply.github.com>
2020-09-24 13:22:19 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // adding commit
 TEST_CASE ("Sum function test", "[Sum]"){
-  double<vector> nums = {1,2,3,4,5};//15
-  REQUIRE( Sum(nums) == 15);
+  double<vector> nums = {1.0,2.0,3.0,4.0,5.0};//15
+  REQUIRE( Sum(nums) == 15.0);
 }
 
