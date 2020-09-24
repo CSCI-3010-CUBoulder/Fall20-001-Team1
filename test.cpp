2e041ed7f0b4a8e60b1c62d53858ea6728c9a7b2
picabotpi <59852986+picabotpi@users.noreply.github.com>
2020-09-24 12:59:34 -0600

    add sum_and_fun test

--- test.cpp
+++ test.cpp
@@ -5,3 +5,15 @@
 
 
 // adding commit
+TES_CASE ("Sum function test", "[Sum]"){
+  double<vector> nums = {1,2,3,4,5}//15
+  REQUIRE( Sum(nums) == 120);
+  double<vector> nums_1 = {2,3,4,5,6}//20
+  REQUIRE( Sum(nums_1) == 720);
+}
+TES_CASE ("Product function test", "[Product]"){
+  double<vector> nums = {1,2,3,4,5}//15
+  REQUIRE( Product(nums) == 120);
+  double<vector> nums_1 = {2,3,4,5,6}//20
+  REQUIRE( Product(nums_1) == 720);
+}
